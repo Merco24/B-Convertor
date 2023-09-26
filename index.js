b0ddeabd87ace51f56a5ebb689b5161d73eb6364
Merco24 <mervicjeazet24@gmail.com>
2023-09-26 22:53:29 +0100

    feat(all):Liaison des fonctions avec le input et affichage du résultat--Merco24

--- index.js
+++ index.js
@@ -27,5 +27,44 @@ function binaryB(decimal) {
     let hexadecimal = "";
 
     hexadecimal = decimal.toString(16);
+
     return hexadecimal;
   }
+
+  //Liaison des boutons 
+  
+  const btnSubmit = document.querySelector("#btn-submit");
+  
+  btnSubmit.addEventListener("click", function(){
+    //Vérifiication !chaine vide
+    const intNum = document.querySelector("#in-num");
+    
+    if(!intNum.value){
+      alert("Place enter the number!!");
+
+    }else if(intNum.value < 0){
+      alert("Error!! Enter the positive number.");
+
+    } else if( intNum.value > 999999999){
+      alert("Enter a lower number 999999999");
+
+    } else{
+      let tempNum = parseInt(intNum.value) ;
+      let resultNum = document.querySelector(".result");
+    
+      if (document.querySelector("#binary").checked ) {
+        resultNum.textContent = binaryB(tempNum);
+
+      }else if(document.querySelector("#octal").checked) {
+        resultNum.textContent = octalB(tempNum);
+        
+      }else if(document.querySelector("#hexadecimal").checked) {
+        resultNum.textContent = hexadecimalB(tempNum);
+      }else{
+        alert("Please!! Enter the base to convert.");
+      }
+
+    }
+  })
+  //Fonction de calcul de la moyenne 
+  
